1a5315b46bdd6dd7139b8a04b79c4c05947b6cc0
ajrwkrndua2-commits <ajrwkrndua2@gmail.com>
2026-03-17 05:02:39 +0900

    Add settings save button

--- Aion2Dashboard/SettingsWindow.xaml.cs
+++ Aion2Dashboard/SettingsWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Input;
 using Aion2Dashboard.ViewModels;
 
 namespace Aion2Dashboard;
@@ -9,6 +10,13 @@ public partial class SettingsWindow : Window
     {
         InitializeComponent();
         DataContext = viewModel;
+    }
+
+    private void Save_Click(object sender, RoutedEventArgs e)
+    {
+        Keyboard.ClearFocus();
+        DialogResult = true;
+        Close();
     }
 
     private void Close_Click(object sender, RoutedEventArgs e)
